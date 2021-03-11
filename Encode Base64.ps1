bd1a95e15ff9a15e00802309036bc8bec267cff2
inobitsca <62749378+inobitsca@users.noreply.github.com>
2021-03-11 08:00:17 +0200

    Update Encode Base64.ps1
    Script Tweaks

--- Encode Base64.ps1
+++ Encode Base64.ps1
@@ -11,3 +11,7 @@ $EncodedText2
 
 $b  = [System.Convert]::FromBase64String($EncodedText)
 [System.Text.Encoding]::UTF8.GetString($b)
+
+$path = "C:\Users\cedri\Pictures\External Email Banner2.jpg"
+$Encoded = [Convert]::ToBase64String((Get-Content -Path $path -Encoding Byte))
+$Encoded > c:\temp\encoded.txt
