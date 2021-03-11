0eac74178404dca0a271b379b36b8a45c3636606
inobitsca <62749378+inobitsca@users.noreply.github.com>
2021-03-11 08:00:28 +0200

    Update New Relay connector.ps1
    Script Tweaks

--- Exchange/New Relay connector.ps1
+++ Exchange/New Relay connector.ps1
@@ -1,8 +1,8 @@
 $relay = Get-ReceiveConnector "srishub\mail relay"
 $range = $relay.RemoteIPRanges
 
-New-Receiveconnector -Name "InternalRelay" -RemoteIPRange ($range) -TransportRole "FrontendTransport" -Bindings ("192.168.30.74:25") -usage "Custom" -Server "SRPKLExchange"
+New-Receiveconnector -Name "InternalRelay" -RemoteIPRange ($range) -TransportRole "FrontendTransport" -Bindings ("172.16.200.6:25") -usage "Custom" -Server "MKJHBEXC001.medikredit.co.za"
 
 Set-ReceiveConnector -identity "InternalRelay" -PermissionGroups "AnonymousUsers"
 
-Get-ReceiveConnector "InternalRelay" | Add-ADPermission -User "NT Authority\ANONYMOUS LOGON" -ExtendedRights "Ms-Exch-SMTP-Accept-Any-Recipient"
+Get-ReceiveConnector "MKJHBEXC001\MDKGateway" | Add-ADPermission -User "NT Authority\ANONYMOUS LOGON" -ExtendedRights "Ms-Exch-SMTP-Accept-Any-Recipient"
